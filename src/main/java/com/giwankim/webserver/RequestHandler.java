409c46864db257988cfb3b07d2780812e4033d09
Giwan Kim <giwan.kim@gmail.com>
2024-03-11 13:53:01 +0900

    feat(webserver): WebServer 뼈대 구현

--- src/main/java/com/giwankim/webserver/RequestHandler.java
+++ src/main/java/com/giwankim/webserver/RequestHandler.java
@@ -1,2 +1,58 @@
-package com.giwankim.webserver;public class RequestHandler {
+package com.giwankim.webserver;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+
+public class RequestHandler extends Thread {
+  private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
+
+  private final Socket connection;
+
+  public RequestHandler(Socket connectionSocket) {
+    this.connection = connectionSocket;
+  }
+
+  @Override
+  public void run() {
+    logger.debug(
+      "New Client Connection! Connected to IP: {}, Port: {}", connection.getInetAddress(), connection.getPort());
+
+    try (
+      InputStream in = connection.getInputStream();
+      OutputStream out = connection.getOutputStream()
+    ) {
+      DataOutputStream dos = new DataOutputStream(out);
+      byte[] body = "Hello, World!".getBytes();
+      response200Header(dos, body.length);
+      responseBody(dos, body);
+    } catch (IOException e) {
+      logger.error(e.getMessage());
+    }
+  }
+
+  private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
+    try {
+      dos.writeBytes("HTTP/1.1 200 OK \r\n");
+      dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
+      dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
+      dos.writeBytes("\r\n");
+    } catch (IOException e) {
+      logger.error(e.getMessage());
+    }
+  }
+
+  private void responseBody(DataOutputStream dos, byte[] body) {
+    try {
+      dos.write(body, 0, body.length);
+      dos.flush();
+    } catch (IOException e) {
+      logger.error(e.getMessage());
+    }
+  }
 }
--- src/main/java/com/giwankim/webserver/WebServer.java
+++ src/main/java/com/giwankim/webserver/WebServer.java
@@ -1,2 +1,32 @@
-package com.giwankim.webserver;public class WebServer {
+package com.giwankim.webserver;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class WebServer {
+  private static final Logger logger = LoggerFactory.getLogger(WebServer.class);
+  private static final int DEFAULT_PORT = 8080;
+
+  public static void main(String... args) throws IOException {
+    int port;
+    if (args == null || args.length == 0) {
+      port = DEFAULT_PORT;
+    } else {
+      port = Integer.parseInt(args[0]);
+    }
+
+    try (ServerSocket listenSocket = new ServerSocket(port)) {
+      logger.info("Web Application Server started on port {}", port);
+
+      Socket connection;
+      while ((connection = listenSocket.accept()) != null) {
+        RequestHandler requestHandler = new RequestHandler(connection);
+        requestHandler.start();
+      }
+    }
+  }
 }
