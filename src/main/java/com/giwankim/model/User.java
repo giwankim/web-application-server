bb91a9b2ec426532b481341a899f101bfe9b89fd
Giwan Kim <giwan.kim@gmail.com>
2024-03-12 10:29:54 +0900

    feat(model): User 도메인 모델을 setter없는 불변객체로 추가

---
+++ src/main/java/com/giwankim/model/User.java
@@ -0,0 +1,36 @@
+package com.giwankim.model;
+
+public class User {
+  private final String userId;
+  private final String password;
+  private final String name;
+  private final String email;
+
+  public User(String userId, String password, String name, String email) {
+    this.userId = userId;
+    this.password = password;
+    this.name = name;
+    this.email = email;
+  }
+
+  public String getUserId() {
+    return userId;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  @Override
+  public String toString() {
+    return String.format("User{userId='%s', password='%s', name='%s', email='%s'}", userId, password, name, password);
+  }
+}
