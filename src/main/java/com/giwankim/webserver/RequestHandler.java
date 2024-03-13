f8e24c5f7d99969e23f07a3fd0cd228f001f7c17
Giwan Kim <giwan.kim@gmail.com>
2024-03-13 13:12:17 +0900

    feat(webserver): GET /user/create

--- src/main/java/com/giwankim/webserver/RequestHandler.java
+++ src/main/java/com/giwankim/webserver/RequestHandler.java
@@ -1,16 +1,26 @@
 package com.giwankim.webserver;
 
+import com.giwankim.model.User;
+import com.giwankim.util.HttpRequestUtils;
+import com.giwankim.util.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.*;
 import java.net.Socket;
-import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
 import java.util.Objects;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
 
 public class RequestHandler extends Thread {
   private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
+  private static final String WEBAPP_PATH = "src/main/webapp";
+  private static final String INDEX_HTML = "/index.html";
 
   private final Socket connection;
 
@@ -27,31 +37,61 @@ public class RequestHandler extends Thread {
       InputStream in = connection.getInputStream();
       OutputStream out = connection.getOutputStream()
     ) {
-      BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(in, StandardCharsets.UTF_8));
+      BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(in, UTF_8));
 
       // request line
       String line = bufferedReader.readLine();
-      logger.debug("request line: {}", line);
-      if (line == null) {
+      if (StringUtils.isNullOrBlank(line)) {
+        return;
+      }
+      logger.debug("request line : {}", line);
+
+      List<String> tokens = Arrays.asList(line.split("\\s+", 3));
+      if (tokens.size() != 3) {
         return;
       }
 
-      String[] tokens = line.split(" ");
-      String uri = tokens[1];
-
       // request headers
-      while (!(line = bufferedReader.readLine()).isBlank()) {
-        logger.debug("header: {}", line);
+      while ((line = bufferedReader.readLine()) != null) {
+        if (line.isBlank()) {
+          break;
+        }
+        logger.debug("header : {}", line);
       }
 
       // response
-      DataOutputStream dos = new DataOutputStream(out);
-      byte[] body = Files.readAllBytes(new File("./src/main/webapp" + uri).toPath());
-      response200Header(dos, body.length);
-      responseBody(dos, body);
+      String url = getUrlOrDefault(tokens);
+      if (url.startsWith("/user/create")) {
+        // query string
+        String queryString = "";
+        if (url.contains("?")) {
+          queryString = url.substring(url.indexOf("?") + 1);
+        }
+        Map<String, String> queryParams = HttpRequestUtils.parseQueryString(queryString);
+        // new user
+        User user = new User(
+          queryParams.get("userId"),
+          queryParams.get("password"),
+          queryParams.get("name"),
+          queryParams.get("email"));
+        logger.debug("User: {}", user);
+      } else {
+        DataOutputStream dos = new DataOutputStream(out);
+        byte[] body = Files.readAllBytes(Paths.get(WEBAPP_PATH, url));
+        response200Header(dos, body.length);
+        responseBody(dos, body);
+      }
     } catch (IOException e) {
       logger.error(e.getMessage());
     }
+  }
+
+  private static String getUrlOrDefault(List<String> tokens) {
+    String url = tokens.get(1);
+    if ("/".equals(url)) {
+      return INDEX_HTML;
+    }
+    return url;
   }
 
   private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
