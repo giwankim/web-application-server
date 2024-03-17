51279d53190b7eb87b837af277e56518b4401cfd
Giwan Kim <giwan.kim@gmail.com>
2024-03-17 18:35:52 +0900

    refactor(webserver): RequestMapping을 통해서 적절한 controller로 라우팅한다.

--- src/main/java/com/giwankim/webserver/RequestHandler.java
+++ src/main/java/com/giwankim/webserver/RequestHandler.java
@@ -1,31 +1,24 @@
 package com.giwankim.webserver;
 
-import com.giwankim.db.Database;
-import com.giwankim.http.HttpCookies;
+import com.giwankim.controller.Controller;
 import com.giwankim.http.HttpRequest;
 import com.giwankim.http.HttpRequestParser;
 import com.giwankim.http.HttpResponse;
-import com.giwankim.model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
-import java.util.Collection;
 import java.util.Objects;
-import java.util.Optional;
+
+import static com.giwankim.webserver.Constants.INDEX;
 
 public class RequestHandler extends Thread {
   private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
-
-  private static final String INDEX_HTML = "/index.html";
-
-  private static final String LOGIN_HTML = "/user/login.html";
-
-  private static final String LOGIN_FAILED_HTML = "/user/login_failed.html";
-
-  private static final String LOGIN_COOKIE_KEY = "login";
 
   private final Socket client;
 
@@ -46,74 +39,20 @@ public class RequestHandler extends Thread {
       HttpRequest request = HttpRequestParser.parse(reader);
       HttpResponse response = HttpResponse.from(writer);
 
-      String path = getPathOrDefault(request.getPath());
-      switch (path) {
-        case "/user/create" -> createUser(request, response);
-        case "/user/login" -> login(request, response);
-        case "/user/list" -> listUsers(request, response);
-        case null, default -> response.forward(path);
+      Controller controller = RequestMapping.getController(request.getPath());
+      if (controller == null) {
+        response.forward(getPathOrDefault(request.getPath()));
+      } else {
+        controller.service(request, response);
       }
     } catch (IOException e) {
       logger.error(e.getMessage());
     }
   }
 
-  private static void listUsers(HttpRequest request, HttpResponse response) {
-    if (!isLoggedIn(request.getCookies())) {
-      response.sendRedirect(LOGIN_HTML);
-      return;
-    }
-    Collection<User> users = Database.findAll();
-    StringBuilder sb = new StringBuilder();
-    sb.append("<table border='1'>");
-    for (User user : users) {
-      sb.append("<tr>");
-      sb.append("<td>").append(user.getUserId()).append("</td>");
-      sb.append("<td>").append(user.getName()).append("</td>");
-      sb.append("<td>").append(user.getEmail()).append("</td>");
-      sb.append("</tr>");
-    }
-    sb.append("</table>");
-    response.forwardBody(sb.toString());
-  }
-
-  private static void login(HttpRequest request, HttpResponse response) {
-    Optional<User> maybeUser = Database.findUserById(request.getParameter("userId"));
-    if (maybeUser.isPresent()) {
-      User user = maybeUser.get();
-      if (user.comparePasswords(request.getParameter("password"))) {
-        response.setHeader("Set-Cookie", LOGIN_COOKIE_KEY + "=true; Path=/");
-        response.sendRedirect(INDEX_HTML);
-      } else {
-        response.sendRedirect(LOGIN_FAILED_HTML);
-      }
-    } else {
-      response.sendRedirect(LOGIN_FAILED_HTML);
-    }
-  }
-
-  private static void createUser(HttpRequest request, HttpResponse response) {
-    User user = new User(
-      request.getParameter("userId"),
-      request.getParameter("password"),
-      request.getParameter("name"),
-      request.getParameter("email"));
-    Database.addUser(user);
-    logger.debug("User : {}", user);
-    response.sendRedirect(INDEX_HTML);
-  }
-
-  private static boolean isLoggedIn(HttpCookies cookies) {
-    String value = cookies.getCookie(LOGIN_COOKIE_KEY);
-    if (value == null) {
-      return false;
-    }
-    return Boolean.parseBoolean(value);
-  }
-
   private static String getPathOrDefault(String url) {
     if ("/".equals(url)) {
-      return INDEX_HTML;
+      return INDEX;
     }
     return url;
   }
